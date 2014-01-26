873d52c3eb975318747fd3bf2866acaa91f7156f
Sehui Park <mike.sayhi@gmail.com>
2014-01-26 16:46:38 -0400

    Add itween

---
+++ Assets/Script/targetmove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class targetmove : MonoBehaviour {
+
+	public Vector3 	startpos;
+	public Vector3 	endpos;
+	public float	speed;
+	
+	// Use this for initialization
+	void Start () {
+		transform.position = startpos;
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		transform.position = Vector3.Lerp( transform.position, endpos, Time.deltaTime * speed );
+	}
+}
