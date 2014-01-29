75212784dd18742d6f50a5c3a6d38e24ac45c9ba
donghoonchoi76 <lynus76@gmail.com>
2014-01-29 06:59:20 -0500

    added earth

---
+++ Assets/Script/earth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class earth : MonoBehaviour {
+    private int iHP;
+
+	// Use this for initialization
+	void Start () {
+        iHP = 100;
+        iTween.RotateBy(gameObject, iTween.Hash("z", 90, "speed", 20, "loopType", "loop", "easeType", "linear", "delay", 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
