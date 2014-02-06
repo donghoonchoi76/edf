fe341c65b1fa8990c62c7105dc4e4f864ac59237
donghoonchoi76 <lynus76@gmail.com>
2014-02-06 06:06:09 -0500

    assigned slot position

--- Assets/Script/earth.cs
+++ Assets/Script/earth.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class earth : MonoBehaviour {
+    public const int MAX_SLOT = 20;
+
     private int iHP;
 
 	// Use this for initialization
 	void Start () {
+        GameObject slot = GameObject.Find("slot0");
+        Vector3 pos = slot.transform.position;
+
+        for (int i = 1; i < MAX_SLOT; i++)
+        {
+            slot = GameObject.Find("slot" + i);
+            slot.transform.position = pos;
+            Vector3 axis = new Vector3(0.0f, 0.0f, 1.0f);
+            Vector3 pibot = new Vector3(0.0f, 0.0f, 0.0f);
+            slot.transform.RotateAround(pibot, axis, i * 18.0f);
+        }
+
+
         iHP = 100;
         iTween.RotateBy(gameObject, iTween.Hash("z", 90, "speed", 20, "loopType", "loop", "easeType", "linear", "delay", 0));
 	}
